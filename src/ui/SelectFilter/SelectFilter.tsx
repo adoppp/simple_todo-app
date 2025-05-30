a4f357a23dc082c0deeb233f9bda808d13989ac8
adop <chdn786od@gmail.com>
2025-05-30 17:45:41 +0200

    refactoring(Filter): filter styles added

--- src/ui/SelectFilter/SelectFilter.tsx
+++ src/ui/SelectFilter/SelectFilter.tsx
@@ -1,32 +1,62 @@
 import type { Filter } from "@/constants/globalConstants";
 import { FormControl, InputLabel, MenuItem, Select, type SelectChangeEvent } from "@mui/material";
+import { indigo, purple } from "@mui/material/colors";
 import { type FC } from "react";
 
 interface SelectFilterProps {
-    currentFilter: Filter,
-    setFilter: (filter: Filter) => void,
+    currentFilter: Filter;
+    setFilter: (filter: Filter) => void;
+}
+
+const menuItemStyles = {
+    '&.Mui-selected': {
+        backgroundColor: purple[200],
+    },
+    '&.Mui-selected:hover': {
+        backgroundColor: purple[400],
+    },
+    '&:hover': {
+        backgroundColor: purple[300],
+    },
 };
 
-export const SelectFilter: FC<SelectFilterProps> = ({ currentFilter, setFilter }) => {
-    const handleFilterChange = (e: SelectChangeEvent) => {
-        setFilter(e.target.value as Filter);
-    };
+const menuPaperStyles = {
+    backgroundColor: indigo[200],
+    border: '2px solid',
+    borderColor: purple[500],
+};
 
-    return ( 
+const selectMenuTypes: Filter[] = ['All', 'Active', 'Completed'];
+
+export const SelectFilter: FC<SelectFilterProps> = ({ currentFilter, setFilter }) => {
+const handleFilterChange = (e: SelectChangeEvent) => {
+    setFilter(e.target.value as Filter);
+};
+
+return (
     <FormControl fullWidth>
-        <InputLabel id="todo-select-label" color="secondary" >All</InputLabel>
-        <Select
+    <InputLabel id="todo-select-label" color="secondary">
+        Filter
+    </InputLabel>
+    <Select
         labelId="todo-select-label"
         id="todo-simple-select"
         color="secondary"
         value={currentFilter}
-        label="Age"
+        label="Filter"
         onChange={handleFilterChange}
-        >
-            <MenuItem value="All">All</MenuItem>
-            <MenuItem value="Active">Active</MenuItem>
-            <MenuItem value="Completed">Completed</MenuItem>
-        </Select>
-        </FormControl>
-    )
-}
+        MenuProps={{
+            PaperProps: {
+            sx: menuPaperStyles,
+            },
+        }}
+    >
+        {selectMenuTypes.map((item) => (
+            <MenuItem key={item} value={item} sx={menuItemStyles}>
+            {item}
+            </MenuItem>
+        ))}
+    </Select>
+    </FormControl>
+);
+};
